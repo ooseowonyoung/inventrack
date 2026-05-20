415614b0400a098bb8901872a3a202cfd5ed70ea
Okto Carlos Agave siahaan <oktosiahaan8008@gmail.com>
2026-05-20 19:58:49 +0700

    feat: setup fillable and relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,11 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'nama_kategori',
+        'deskripsi',
+        'image',
+    ];
 }
